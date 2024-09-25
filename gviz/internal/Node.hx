55fb43c40eb1293d94af77f4d2cc05587e4ea162
Asher Hoeveler <dev@crinfarr.io>
2024-09-24 19:01:50 -0500

    Better node class

--- gviz/internal/Node.hx
+++ gviz/internal/Node.hx
@@ -11,13 +11,20 @@ using StringTools;
 @:haxe.warning('-Wdeprecated')
 class Node {
 	#if cpp
+	private final parent:Graph;
+    @:allow(gviz.internal.Edge)
 	private final node:NodeWrapper;
-    
-    private final style:Array<String> = [];
 
-	public function new(wrapped:NodeWrapper) {
-		this.node = wrapped;
+	private final style:Array<String> = [];
+
+    @:allow(gviz.internal.Graph)
+	private function new(name:String, parent:Graph) {
+        this.parent = parent;
+        this.node = parent.intGraph.createNode(name);
 	}
+    public function connect(node:Node, edgeName:String) {
+        return parent.connect(this, node, edgeName);
+    }
 
 	public function setAttr(att:NodeAttribute) {
 		switch (att) {
@@ -40,31 +47,31 @@ class Node {
 					case ByHex(hex):
 						hex;
 					case ByRGB(r, g, b):
-                        if (r > 0xff || g > 0xff || b > 0xff)
-                            throw new ValueException('Invalid RGB value: R:$r G:$g B:$b');
+						if (r > 0xff || g > 0xff || b > 0xff)
+							throw new ValueException('Invalid RGB value: R:$r G:$g B:$b');
 						"#" + (r << 16 & g << 8 & b).hex();
 				});
-            case Layer(overlaytype):
-                this.node.setAttr('layer', switch (overlaytype) {
-                    case All:
-                        "all";
-                    case ByID(id):
-                        id;
-                    case IDRange(startID, endID):
-                        '$startID:$endID';
-                });
-            case Style(styletype):
-                for (inst in styletype) {
-                    switch (inst) {
-                        case Tapered:
-                            throw new ValueException('Tapered style can only be used on edges');
-                        case _:
-                            while (this.style.length != 0)
-                                this.style.pop();
-                            style.push(inst.getName().toLowerCase());
-                    }
-                }
-                this.node.setAttr('style', this.style.join(','));
+			case Layer(overlaytype):
+				this.node.setAttr('layer', switch (overlaytype) {
+					case All:
+						"all";
+					case ByID(id):
+						id;
+					case IDRange(startID, endID):
+						'$startID:$endID';
+				});
+			case Style(styletype):
+				for (inst in styletype) {
+					switch (inst) {
+						case Tapered:
+							throw new ValueException('Tapered style can only be used on edges');
+						case _:
+							while (this.style.length != 0)
+								this.style.pop();
+							style.push(inst.getName().toLowerCase());
+					}
+				}
+				this.node.setAttr('style', this.style.join(','));
 			case _:
 				this.node.setAttr(att.getName().toLowerCase(), '${att.getParameters()[0]}');
 		}
