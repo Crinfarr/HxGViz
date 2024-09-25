a518dce9576753f310416dd51e347e88562870da
Asher Hoeveler <dev@crinfarr.io>
2024-09-24 19:01:31 -0500

    Graph can create edges and nodes

--- gviz/internal/Graph.hx
+++ gviz/internal/Graph.hx
@@ -6,12 +6,15 @@ import haxe.Exception;
 import gviz.extern.cpp.GraphWrapper;
 import gviz.internal.Enums.GraphType;
 import gviz.internal.Enums.GraphAttribute;
+import gviz.internal.Node;
 import gviz.extern.web.GraphLayer;
 
 using haxe.EnumTools.EnumValueTools;
 
 class Graph {
     #if cpp
+    @:allow(gviz.internal.Node)
+    @:allow(gviz.internal.Edge)
     final intGraph:gviz.extern.cpp.GraphWrapper;
 	private static function parseGtype(type:GraphType):String {
 		switch (type) {
@@ -26,15 +29,20 @@ class Graph {
 	public function new(graphname:String = "", graphType:GraphType) {
 		intGraph = GraphWrapper.create(graphname, parseGtype(graphType));
     }
-    public function createNode(nodename:String=""):Node {
-        final wrapper:NodeWrapper = intGraph.createNode(nodename);
-        final node = new Node(wrapper);
-        return node;
+    public function addNode(name:String):Node {
+        return new Node(name, this);
     }
-    public function createEdge(edgename:String="", nodeA:NodeWrapper, nodeB:NodeWrapper) {
-        final edge:EdgeWrapper = intGraph.createEdge(nodeA, nodeB, edgename);
-        //TODO edge interface Edge class
+    public function connect(nodeA:Node, nodeB:Node, edgeName:String):Edge {
+        return new Edge(nodeA, nodeB, edgeName, this);
     }
+    // public function createNode(nodename:String=""):Node {
+    //     // final wrapper:NodeWrapper = intGraph.createNode(nodename);
+    //     final node = new Node(this);
+    //     return node;
+    // }
+    // public function createEdge(edgename:String="", nodeA:NodeWrapper, nodeB:NodeWrapper) {
+    //     final edge:EdgeWrapper = intGraph.createEdge(nodeA, nodeB, edgename);
+    // }
     public function setAttribute(attribute:GraphAttribute) {
         switch (attribute) {
             case ClusterRank(enumval):
